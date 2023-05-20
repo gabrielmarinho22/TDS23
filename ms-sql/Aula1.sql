e74a7e842d3c4d635117d7c1925fe610661aad4c
GABRIEL MARINHO GUIMARÃES <3316.343@edu.fb.org.br>
2023-05-19 21:56:51 -0300

    texto

---
+++ ms-sql/Aula1.sql
@@ -0,0 +1,12 @@
+USE AdventureWorks2012
+
+SELECT* FROM sys.tables
+
+SELECT* FROM Person.Person
+
+SELECT Firstname FROM Person.Person
+
+SELECT FirstName AS PrimeiroNome,
+LastName AS UltimoNome
+FROM Person.Person
+
